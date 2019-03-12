426fdda0b32ba8b47c604f37f76a52b04b69c8f3
rm <sirycity2018@gmail.com>
2019-03-13 00:03:33 +0900

    contentful大体理解

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,23 @@
 <template>
   <div>
-    <!-- render data of the person -->
-    <ul>
-      <li v-for="person in people" :key="person.fields.name">
-      {{ person.fields.name }}
-      </li>
-    </ul>
-    <!-- render blog posts -->
-    <ul>
-      <li v-for="post in posts" :key="post.fields.title">
-        {{ post.fields.title }}
-      </li>
-    </ul>
+    <div v-for="post in posts" :key="post.fields.title">
+      {{ post.fields.title }}
+    </div>
   </div>
 </template>
 
 <script>
   import {createClient} from '~/plugins/contentful.js'
 
-  const client = createClient()
-
   export default {
-    // `env` is available in the context object
+
     async asyncData({env}){
-      const content = await Promise.all([
-        client.getEntries({
-          'sys.id': env.CTF_PERSON_ID
-        }),
-        client.getEntries({
-          'content_type': env.CTF_BLOG_POST_TYPE_ID,
-          order: '-sys.createdAt'
-        })
-      ])
+      const content = await createClient().getEntries({
+        'content_type': env.CTF_BLOG_POST_TYPE_ID,
+        order: '-sys.createdAt',
+      })
 
-      console.dir(content)
-
-      return {
-        people: content[0].items,
-        posts: content[1].items
-      }
+      return { posts: content.items}
 
     }
   }
