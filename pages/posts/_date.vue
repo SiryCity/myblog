afddecdf2dbbe9692cc4c5a64901e07623f35aad
rm <sirycity2018@gmail.com>
2019-03-13 17:17:42 +0900

    link追加

--- pages/posts/_date.vue
+++ pages/posts/_date.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
-  div {{getThisPost()}}
+  div
+    h1 {{getPosts().curr.title}}
+    div {{getPosts().curr.date}}
+    div(v-for='tag in getPosts().curr.tags'): div {{tag}}
+    div {{getPosts().curr.body}}
+    nuxt-link(v-if='getPosts().prev' :to='{name:"posts-date", params:{date: getPosts().prev.date}}')
+      div {{getPosts().prev.title}}
+    nuxt-link(v-if='getPosts().next' :to='{name:"posts-date", params:{date: getPosts().next.date}}')
+      div {{getPosts().next.title}}
+    nuxt-link(to='../') home
 </template>
 
 <script>
@@ -15,12 +24,16 @@
       return {posts: contents.items.map(item => item.fields)}
     },
     methods:{
-      getThisPost(){
+      getPosts(){
         return this.posts.reduce((... post) =>
           post[0]
           || post[1].date !== this.$route.params.date
             ? post[0]
-            : post[1]
+            : {
+              next: post[3][post[2] - 1] || null,
+              curr: post[1],
+              prev: post[3][post[2] + 1] || null
+            }
         , null)
       }
     }
