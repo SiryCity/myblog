ea8f4605807929fde2930d2b737d00cf4d9c231f
rm <sirycity2018@gmail.com>
2019-03-20 15:36:25 +0900

    complete api

--- components/LargePosts.vue
+++ components/LargePosts.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
 section-wrapper(title='BLOG')
-  div.post-large(
+  nuxt-link.post-large(
+    :to='{name:"posts-date", params:{date: post.date}}'
     v-for='(post, i) in posts',
     :key='`post-large${i}`',
     :class='{"post--notfirst": i}'
   )
     img(src='~/assets/icon.svg')
-    nuxt-link(:to='{name:"posts-date", params:{date: post.date}}')
+    div
       p.post-large__tag {{post.tags[0]}}
       p.post-large__date {{post.date.replace(/-/g, '/')}}
       p.post-large__title {{post.title}}
@@ -28,20 +29,21 @@ export default {
   padding 5px 0
   box-sizing border-box
   display flex
+  align-items center
+
+  text-decoration none
+  &:hover
+    background-color #D9F0F7
   img
     width 60px
     height 60px
     border 1px solid #cccccc
     box-sizing border-box
-  a
+  div
     width calc(100% - 64px)
-    height 60px
     display block
     padding-left 4px
     color #09c
-    text-decoration none
-    &:hover
-      background-color #D9F0F7
     .post-large__tag
       color #173759
     .post-large__date
