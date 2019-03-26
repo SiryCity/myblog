35ccc875a45171ad2bfeba442c66a481b5eab3e1
rm <sirycity2018@gmail.com>
2019-03-26 13:34:23 +0900

    APIの一部を削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,8 +97,8 @@ module.exports = {
   env: {
     CTF_SPACE_ID: config.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
-    CTF_PERSON_ID: config.CTF_PERSON_ID,
     CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID,
+    
     FORMS_API: process.env.FORMS_API
   }
 }
